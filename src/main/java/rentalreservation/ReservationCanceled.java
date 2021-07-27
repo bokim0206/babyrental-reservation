fdb7c17c5f4fd20ef6fe3dc7ead1eae88251ad22
bokim0206 <85722703+bokim0206@users.noreply.github.com>
2021-07-27 09:38:49 +0900

    Add files via upload

---
+++ src/main/java/rentalreservation/ReservationCanceled.java
@@ -0,0 +1,70 @@
+package rentalreservation;
+
+public class ReservationCanceled extends AbstractEvent {
+
+    private Long id;
+    private Long rentalId;
+    private String rentalName;
+    private String rentalStatus;
+    private String rentalType;
+    private String rentalPeriod;
+    private Float rentalPrice;
+    private String memberName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getRentalId() {
+        return rentalId;
+    }
+
+    public void setRentalId(Long rentalId) {
+        this.rentalId = rentalId;
+    }
+    public String getRentalName() {
+        return rentalName;
+    }
+
+    public void setRentalName(String rentalName) {
+        this.rentalName = rentalName;
+    }
+    public String getRentalStatus() {
+        return rentalStatus;
+    }
+
+    public void setRentalStatus(String rentalStatus) {
+        this.rentalStatus = rentalStatus;
+    }
+    public String getRentalType() {
+        return rentalType;
+    }
+
+    public void setRentalType(String rentalType) {
+        this.rentalType = rentalType;
+    }
+    public String getRentalPeriod() {
+        return rentalPeriod;
+    }
+
+    public void setRentalPeriod(String rentalPeriod) {
+        this.rentalPeriod = rentalPeriod;
+    }
+    public Float getRentalPrice() {
+        return rentalPrice;
+    }
+
+    public void setRentalPrice(Float rentalPrice) {
+        this.rentalPrice = rentalPrice;
+    }
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+}
